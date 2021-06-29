253fbbd81af5f286035f898fae57bbfb2ec82f32
kamilkow1123 <22kamil.kow@gmail.com>
2021-06-29 22:22:10 +0200

    New components: Header and HeroSection

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Header from './components/Header';
+import HeroSection from './components/HeroSection';
 
 function App() {
-	return <div className="App">App</div>;
+	return (
+		<div>
+			<Header />
+			<HeroSection />
+		</div>
+	);
 }
 
 export default App;
