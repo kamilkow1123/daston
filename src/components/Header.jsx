c4d40b8250bb0dab705471c8f7ff3886621079ba
kamilkow1123 <22kamil.kow@gmail.com>
2021-07-01 12:21:45 +0200

    Adding elements to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { FaBars } from 'react-icons/fa';
+import { Link as LinkS } from 'react-scroll';
 
 const Header = () => {
-	return <div>Header</div>;
+	return (
+		<nav>
+			<div className="logo">DASTON | DAWID KOWALEWSKI</div>
+			<div className="mobile-icon">
+				<FaBars />
+			</div>
+			<ul className="nav-menu">
+				<li className="nav-item">
+					<LinkS>Portfolio</LinkS>
+				</li>
+				<li className="nav-item">
+					<LinkS>Firma</LinkS>
+				</li>
+				<li className="nav-item">
+					<LinkS>Kontakt</LinkS>
+				</li>
+			</ul>
+		</nav>
+	);
 };
 
 export default Header;
