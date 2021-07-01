a45e2961ee008bb1f51f9d34afee7d0710cadd65
kamilkow1123 <22kamil.kow@gmail.com>
2021-07-01 23:21:10 +0200

    Styling hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const HeroSection = () => {
-	return <div>Hero</div>;
+	return (
+		<div className="home">
+			<div className="home-content">
+				<h1>DASTON</h1>
+				<p>REALISTYCZNE WIZUALIZACJE</p>
+			</div>
+		</div>
+	);
 };
 
 export default HeroSection;
