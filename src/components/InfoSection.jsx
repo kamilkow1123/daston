eb169381a5e9f6e784766b4fa73f8919b2b3c8ef
kamilkow1123 <22kamil.kow@gmail.com>
2021-07-04 18:15:49 +0200

    Styling info section

--- src/components/InfoSection.jsx
+++ src/components/InfoSection.jsx
@@ -2,8 +2,17 @@ import React from 'react';
 
 const InfoSection = () => {
 	return (
-		<div>
-			<h1>Info</h1>
+		<div className="info-section">
+			<div className="info-section-image">
+				<div className="info-section-content">
+					<h1>Firma</h1>
+					<p>
+						Firma Daston specjalizuje się w tworzeniu realistycznych wizualizacji budynków jak i ich wnętrz.
+						Wizualizacje tworzone są indywidualnie na potrzeby klienta.
+					</p>
+					<div className="info-section-content-button">dowiedz się więcej</div>
+				</div>
+			</div>
 		</div>
 	);
 };
