ed6c2b75fc38aaa322be9592a2fc218acf209a48
kamilkow1123 <22kamil.kow@gmail.com>
2021-07-04 23:21:03 +0200

    Responsive footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,43 @@
 import React from 'react';
+import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa';
 
 const Footer = () => {
-	return <div style={{ height: '200px' }}>Footer</div>;
+	return (
+		<div className="footer">
+			<div className="footer-container">
+				<div className="footer-logo">DASTON</div>
+				<div className="footer-rights">
+					All rights reserved &copy; {new Date().getFullYear()} By Kamil Kowalewski
+				</div>
+				<div className="footer-social">
+					<a
+						className="footer-social-link"
+						href="//www.facebook.com/dawid.kowalewski.12/"
+						target="_blank"
+						aria-label="Facebook"
+					>
+						<FaFacebook />
+					</a>
+					<a
+						className="footer-social-link"
+						href="//www.instagram.com/daston_render/"
+						target="_blank"
+						aria-label="Instagram"
+					>
+						<FaInstagram />
+					</a>
+					<a
+						className="footer-social-link"
+						href="//www.linkedin.com/in/dawid-kowalewski-109073202/"
+						target="_blank"
+						aria-label="Linkedin"
+					>
+						<FaLinkedin />
+					</a>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Footer;
