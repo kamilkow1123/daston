ce82ecd15e6b254f36627ed2c79ae079ccf1eafb
kamilkow1123 <22kamil.kow@gmail.com>
2021-07-05 20:38:33 +0200

    Styling the contact form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -4,20 +4,27 @@ const ContactForm = () => {
 	return (
 		<form className="contact-section-form">
 			<div className="contact-section-form-group">
-				<label htmlFor="firstname">Imię: </label>
+				<label htmlFor="firstname">Imię</label>
 				<input type="text" name="firstname" id="firstname" />
 			</div>
 			<div className="contact-section-form-group">
-				<label htmlFor="lastname">Nazwisko: </label>
+				<label htmlFor="lastname">Nazwisko</label>
 				<input type="text" name="lastname" id="lastname" />
 			</div>
 			<div className="contact-section-form-group">
-				<label htmlFor="email">E-mail: </label>
+				<label htmlFor="email">E-mail</label>
 				<input type="email" name="email" id="email" />
 			</div>
 			<div className="contact-section-form-group">
-				<label htmlFor="phone">Numer telefonu: </label>
+				<label htmlFor="phone">Numer telefonu</label>
 				<input type="tel" name="phone" id="phone" />
+			</div>
+			<div className="contact-section-form-group">
+				<label>Wiadomość</label>
+				<textarea id="message" type="text" autocomplete="off" />
+			</div>
+			<div className="button-wrapper">
+				<input type="submit" value="WYŚLIJ" className="submit-button" />
 			</div>
 		</form>
 	);
