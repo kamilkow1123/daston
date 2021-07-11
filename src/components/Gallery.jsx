0e8f7a597a8878253acb6440245c5f03e954ce20
kamilkow1123 <22kamil.kow@gmail.com>
2021-07-11 20:05:26 +0200

    Simple lightbox in the gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaTimes } from "react-icons/fa";
 
 const Gallery = ({ photos }) => {
+  const [ lightbox, setLightbox ] = useState(false);
+  const [ tempImgSrc, setTempImgSrc ] = useState("");
+  const getImage = imgSrc => {
+    setTempImgSrc(imgSrc);
+    setLightbox(true);
+  };
+
   return (
-    <div className="gallery">
-      {photos.map((photo, index) => {
-        return (
-          <div className="gallery-img" key={index}>
-            <img src={photo.imgSrc} alt={photo.alt} key={index} />
-          </div>
-        );
-      })}
+    <div>
+      <div className={lightbox ? "lightbox open" : "lightbox"}>
+        <img src={tempImgSrc} />
+        <div className="close-icon" onClick={() => setLightbox(false)}>
+          <FaTimes />
+        </div>
+      </div>
+
+      <div className="gallery">
+        {photos.map((photo, index) => {
+          return (
+            <div
+              className="gallery-img"
+              key={index}
+              onClick={() => getImage(photo.imgSrc)}
+            >
+              <img src={photo.imgSrc} alt={photo.alt} key={index} />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
