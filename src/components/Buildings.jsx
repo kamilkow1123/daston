eee5af5bb859d87ca8d010eb4b7d1ee203e83e7d
kamilkow1123 <22kamil.kow@gmail.com>
2021-07-11 21:04:33 +0200

    Styling lightbox in gallery

--- src/components/Buildings.jsx
+++ src/components/Buildings.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Gallery from "./Gallery";
+import Footer from "./Footer";
 import { BuildingsData } from "./BuildingsData";
 
 const Buildings = () => {
@@ -9,6 +10,7 @@ const Buildings = () => {
       <Navbar visibleMenu={false} alwaysSolid={true} />
       <h1 className="portfolio-page-header">Budynki</h1>
       <Gallery photos={BuildingsData} />
+      <Footer />
     </div>
   );
 };
