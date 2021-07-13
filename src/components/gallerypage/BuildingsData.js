255b22a2b118795e69984f3e85a5273be5693e69
kamilkow1123 <22kamil.kow@gmail.com>
2021-07-13 14:38:00 +0200

    Adding new renders to buildings

--- src/components/gallerypage/BuildingsData.js
+++ src/components/gallerypage/BuildingsData.js
@@ -8,6 +8,10 @@ import BuildingImg7 from "../../img/building7.jpg";
 import BuildingImg8 from "../../img/building8.jpg";
 import BuildingImg9 from "../../img/building9.jpg";
 import BuildingImg10 from "../../img/building10.jpg";
+import BuildingImg11 from "../../img/building11.jpg";
+import BuildingImg12 from "../../img/building12.jpg";
+import BuildingImg13 from "../../img/building13.jpg";
+import BuildingImg14 from "../../img/building14.jpg";
 import BuildingImg1min from "../../img/building1min.jpg";
 import BuildingImg2min from "../../img/building2min.jpg";
 import BuildingImg3min from "../../img/building3min.jpg";
@@ -18,6 +22,10 @@ import BuildingImg7min from "../../img/building7min.jpg";
 import BuildingImg8min from "../../img/building8min.jpg";
 import BuildingImg9min from "../../img/building9min.jpg";
 import BuildingImg10min from "../../img/building10min.jpg";
+import BuildingImg11min from "../../img/building11min.jpg";
+import BuildingImg12min from "../../img/building12min.jpg";
+import BuildingImg13min from "../../img/building13min.jpg";
+import BuildingImg14min from "../../img/building14min.jpg";
 
 export const BuildingsData = [
   {
@@ -68,6 +76,26 @@ export const BuildingsData = [
   {
     imgSrc : BuildingImg10,
     imgMin : BuildingImg10min,
+    alt    : "budynek10",
+  },
+  {
+    imgSrc : BuildingImg11,
+    imgMin : BuildingImg11min,
+    alt    : "budynek10",
+  },
+  {
+    imgSrc : BuildingImg12,
+    imgMin : BuildingImg12min,
+    alt    : "budynek10",
+  },
+  {
+    imgSrc : BuildingImg13,
+    imgMin : BuildingImg13min,
+    alt    : "budynek10",
+  },
+  {
+    imgSrc : BuildingImg14,
+    imgMin : BuildingImg14min,
     alt    : "budynek10",
   },
 ];
